fe5e3077ef0ed7097089d5b35b7fb5be1c152f70
nhrnhr0 <nhrnhr0@gmail.com>
2024-01-12 03:50:42 +0200

    first commit

---
+++ gameManager.gd
@@ -0,0 +1,97 @@
+extends Node2D
+
+
+var selectedPlayer: Node2D;
+var players: Array[Node2D];
+const SELECT_RADIUS = 75;
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#selectedPlayer = get_node("player");
+	players.append(get_node("player1"));
+	#players.append(get_node("player2"));
+	pass # Replace with function body.
+
+func set_selected_player(idx):
+	if selectedPlayer:
+		selectedPlayer.deselect();
+	selectedPlayer = players[idx];
+	selectedPlayer.select();
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	var mouse_pos = get_local_mouse_position()
+	if(Input.is_action_just_pressed("goto_click")):
+		var just_selected = false;
+		for i in players.size():
+			var player = players[i];
+			if (player.position.distance_to(mouse_pos) < SELECT_RADIUS):
+				#selectedPlayer = player;
+				set_selected_player(i);
+				just_selected = true;
+				break;
+		if selectedPlayer && !just_selected:
+			# check if we click any selectedPlayer.player_moves()
+			var remove_from_idx = -1;
+			for i in selectedPlayer.players_moves.size():
+				if(check_if_hovering_point(selectedPlayer.players_moves[i], mouse_pos)):
+					# remove all the move farward
+					remove_from_idx = i;
+					break;
+			
+			if remove_from_idx != -1:
+				selectedPlayer.players_moves = selectedPlayer.players_moves.slice(0, remove_from_idx);
+			else:
+				selectedPlayer.players_moves.append(mouse_pos);
+				selectedPlayer.players_attacks.clear();
+	
+	if(Input.is_action_just_pressed("attack_click")):
+		if selectedPlayer:
+			selectedPlayer.set_fire_target(mouse_pos);
+	# check is the mouse is hovering over the selectedPlayer moves we set hoverable_move_cancel_index
+	if selectedPlayer:
+		var hover_index = -1;
+		for i in selectedPlayer.players_moves.size():
+			if(check_if_hovering_point(mouse_pos, selectedPlayer.players_moves[i])):
+				hover_index = i;
+		selectedPlayer.hoverable_move_cancel_index = hover_index;
+	queue_redraw()
+	
+func check_if_hovering_point(p1: Vector2, p2:Vector2, rad=35):
+	if p1.distance_to(p2) < rad:
+		return true;
+	return false;
+	pass
+func _draw():
+	if(!selectedPlayer):
+		return;
+	#draw_circle(selectedPlayer.position, SELECT_RADIUS,Color(0.3,0.7,0.3,0.5))
+	var label = Label.new()
+	var font = label.get_theme_default_font()
+	#print(selectedPlayer.players_moves.size());
+	# for each of the moves in the array, draw a circle at that position with the numbwer of the move inside, then a line between the circles to show the order
+	for i in selectedPlayer.players_moves.size():
+		var mov_pos = selectedPlayer.players_moves[i];
+		draw_circle(mov_pos, 35, Color(1, 1, 1));
+		var re_center = mov_pos;
+		re_center.x -= 35;
+		re_center.y += 30;
+		var ch = str(i);
+		if selectedPlayer.hoverable_move_cancel_index != -1 && selectedPlayer.hoverable_move_cancel_index <= i:
+			ch = 'X';
+		draw_string(font, re_center, ch,HORIZONTAL_ALIGNMENT_CENTER,72, 72, Color(0,0,0,1))
+		if i > 0:
+			draw_line(mov_pos, selectedPlayer.players_moves[i-1], Color(0,1,0),4);
+		draw_line(selectedPlayer.position, selectedPlayer.players_moves[0], Color(0,1,0),4);
+		
+	# draw attacks:
+	for i in selectedPlayer.players_attacks.size():
+		var attack_pos = selectedPlayer.players_attacks[i];
+		draw_circle(attack_pos, 35, Color(1, 0, 0,0.4));
+		var re_center = attack_pos;
+		re_center.x -= 35;
+		re_center.y += 30;
+		var ch = str(i);
+		draw_string(font, re_center, ch,HORIZONTAL_ALIGNMENT_CENTER,72, 72, Color(0,0,0,1))
+		#if i > 0:
+			#draw_line(attack_pos, selectedPlayer.players_attacks[i-1], Color(1,0,0),4);
+		#draw_line(selectedPlayer.position, selectedPlayer.players_attacks[0], Color(1,0,0),4);
---
+++ player.gd
@@ -0,0 +1,90 @@
+extends CharacterBody2D
+const SELECT_RADIUS = 75;
+var players_moves: Array[Vector2] = [];
+var players_attacks: Array[Vector2] = [];
+var hoverable_move_cancel_index = -1;
+var move_speed = 50;
+var rotate_speed = 0.001;
+var turret_rotate_speed = 0.4;
+var my_name;
+var _is_selected = false;
+
+
+@export var MAX_FIRE_RANGE = 360;
+@export var MIN_FIRE_RANGE = 50;  
+func _ready():
+	pass
+func _draw():
+	pass
+	if _is_selected:
+		draw_circle(Vector2(0,0), SELECT_RADIUS,Color(0.3,0.7,0.3,0.5))
+		#draw_circle(Vector2(0,0), MAX_FIRE_RANGE,)
+		var points = 50;
+		if MAX_FIRE_RANGE:
+			draw_arc(Vector2(0,0), MAX_FIRE_RANGE/2, 0, TAU, points, Color(1,0,0))
+		if MIN_FIRE_RANGE:
+			draw_arc(Vector2(0,0), MIN_FIRE_RANGE/2, 0, TAU, points, Color(1,0,0))
+
+func select():
+	_is_selected=true;
+func deselect():
+	_is_selected=false;
+	
+func set_fire_target(pos):
+	# make sure the point is in the givven range
+	var distance = position.distance_to(pos);
+	#print('fire distance: ', distance, 'current pos: ', position, ' target: ', pos);
+	if distance > MAX_FIRE_RANGE or distance < MIN_FIRE_RANGE:
+		return;
+	players_attacks.append(pos);
+	players_moves.clear();
+	
+	pass
+#func  update_turret(delta):
+func update_turret(delta):
+	if players_attacks.size() > 0:
+		var attack_pos = players_attacks[0]
+		var turret = get_node("turret")
+		
+		# Calculate the angle from the player to the attack position
+		var target_angle = (attack_pos - position).angle()
+		target_angle -= rotation;
+		# Adjust the target angle by subtracting the player's rotation
+		#var player_rotation_radians = deg_to_rad(rotation)
+		# Add the player's rotation to the turret's rotation
+		#print('player_rotation_radians ',player_rotation_radians,' turret.rotation ',turret.rotation,' target_angle ',target_angle, ' diff ', angle_difference(turret.rotation, target_angle), 'rotatio: ', rotation)
+		#target_angle += player_rotation_radians
+		# Use lerp_angle to smoothly interpolate between the current turret rotation and the target angle
+		turret.rotation = lerp_angle(turret.rotation, target_angle, turret_rotate_speed * delta)
+
+		# Check if the turret is close enough to the target angle, and remove the attack from the list
+		if abs(angle_difference(turret.rotation, target_angle)) < 0.1:
+			players_attacks.remove_at(0)
+
+func _physics_process(delta):
+	update_position(delta);
+	update_turret(delta);
+	queue_redraw();
+
+
+func update_position(delta):
+	# if there are moves in the array, move the player to the first move in the array
+	# we do it by first rotating the player to face the move, then moving the player towards the move
+	if players_moves.size() == 0:
+		return;
+	var move = players_moves[0];
+	# rotate the player to face the move
+	var target_angle = (move - position).normalized().angle()
+	rotation = lerp_angle(rotation, target_angle, 0.5 * delta)
+
+
+	if abs(angle_difference(rotation, target_angle)) < 0.1:
+		# move the player towards the move
+		var move_dir = (move - position).normalized();
+		velocity = move_dir * move_speed;
+		move_and_slide()
+		# if the player is close enough to the move, remove the move from the array
+		if (move - position).length() < 10:
+			print('I made it to point: ', players_moves[0])
+			players_moves.remove_at(0);
+			velocity = Vector2(0,0);
